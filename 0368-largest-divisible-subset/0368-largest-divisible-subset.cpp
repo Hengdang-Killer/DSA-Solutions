988eb3ee6d98c927101a8f7690a6b5401538547e
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-02-09 15:22:13 +0530

    Time: 15 ms (80.09%), Space: 11.5 MB (11.66%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -8,35 +8,26 @@ public:
         cout.tie(0);
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> par[n], nxt(n), sz(n, 0);
+        vector<int> par(n), sz(n, 1);
+        int mx = 0, idx;
         for(int i = 0; i < n; i++)
         {
-            int mn = 1e9+7;
             for(int j = i+1; j < n; j++)
             {
                 if(nums[j] % nums[i] == 0)
                 {
-                    mn = min(mn, nums[j]), par[j].push_back(i);
+                    if(sz[j] < sz[i] + 1)
+                        sz[j] = sz[i]+1, par[j] = i;
                 }
-            }
-        }
-        int mx = -1, idx;
-        for(int i = n-1; i >= 0; i--)
-        {
-            for(auto itr : par[i])
-            {
-                if(sz[itr] < sz[i] + 1)
-                    sz[itr] = sz[i]+1, nxt[itr] = i;
             }
             if(sz[i] >= mx)
                 mx = sz[i], idx = i;
         }
-        mx++;
         vector<int> ans;
         while(mx--)
         {
             ans.push_back(nums[idx]);
-            idx = nxt[idx];
+            idx = par[idx];
         }
         return ans;
     }
