82485495d1120e9539cdabadaec34dae5afcb705
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-02-11 12:04:35 +0530

    Time: 26 ms (98.52%), Space: 12.6 MB (76.92%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,3 +1,6 @@
+// T.C. = O(n*n*n)
+// S.C. = O(n*n*n) + O(n)
+/*
 class Solution 
 {
 private:
@@ -34,5 +37,52 @@ public:
         n = grid.size(), m = grid[0].size();
         memset(dp, -1, sizeof(dp));
         return recursion(0, 0, m-1, grid);
+    }
+};
+*/
+
+class Solution 
+{
+public:
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int n = grid.size(), m = grid[0].size();
+        int dp[n][m][m];
+        for(int i = n-1; i >= 0; i--)
+        {
+            for(int j1 = 0; j1 < m; j1++)
+            {
+                for(int j2 = 0; j2 < m; j2++)
+                {
+                    if(i == n-1)
+                    {
+                        dp[i][j1][j2] = grid[i][j1] + grid[i][j2];
+                        if(j1 == j2)
+                            dp[i][j1][j2] -= grid[i][j2];
+                    }
+                    else
+                    {
+                        int move = 0;
+                        for(int p = -1; p <= 1; p++)
+                        {
+                            for(int q = -1; q <= 1; q++)
+                            {
+                                if(j1 + p >= 0 && j1 + p < m && j2 + q >= 0 && j2 + q < m)
+                                {
+                                    move = max(move, dp[i+1][j1+p][j2+q]);
+                                }
+                            }
+                        }
+                        dp[i][j1][j2] = move + grid[i][j1] + grid[i][j2];
+                        if(j1 == j2)
+                            dp[i][j1][j2] -= grid[i][j2];
+                    }
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
