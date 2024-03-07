d264c6338179702573f2fd4c9909c8b8aed20b3b
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-03-07 11:51:53 +0530

    Time: 2 ms (44.73%), Space: 8.7 MB (7.87%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -14,16 +14,19 @@ public:
     ListNode* middleNode(ListNode* head) 
     {
         ios_base::sync_with_stdio(0);
-        if(!head)
-            return head;
         int n = 0;
         ListNode *temp=head;
         while(temp != 0)
-            temp = temp->next, n++;
+        {
+            temp = temp->next;
+            n++;
+        }
         n = n/2;
         temp = head;
         while(n--)
+        {
             temp = temp->next;
+        }
         return temp;
     }
 };
