80b6c910de35d28f8b942cc1ff0d1b72bc32771a
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-03-09 12:28:12 +0530

    Time: 38 ms (98.93%), Space: 52.6 MB (86.28%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int i = 0, j = 0;
         while(i < nums1.size() && j < nums2.size())
         {
