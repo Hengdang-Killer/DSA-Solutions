ed7c6889b03490b54110ed61c9cdf60e010ed47d
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-03-13 11:04:51 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (15.68%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -6,11 +6,11 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        int l = 1, r = n, total = n*(n+1)/2;
+        int l = 1, r = n;
         while(l <= r)
         {
             int mid = l + (r-l)/2;
-            int sum1 = (mid*(mid+1))/2, sum2 = total - (mid*(mid-1))/2;
+            int sum1 = (mid*(mid+1))/2, sum2 = (n*(n+1))/2 - (mid*(mid-1))/2;
             if(sum1 == sum2)
                 return mid;
             else if(sum1 < sum2)
