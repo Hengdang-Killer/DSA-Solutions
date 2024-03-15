7cca6e83b0d1cbb591edc81abbd8f8bdf6e067f3
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-03-15 23:23:05 +0530

    Time: 14 ms (74.02%), Space: 26.2 MB (86.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -6,15 +6,24 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        int n = nums.size(), cnt = 0;
+        int n = nums.size(), cnt = 0, p = 1, idx;
         vector<int> ans(n, 0);
-        for(auto &itr : nums)
+        for(int i = 0; i < n; i++)
         {
-            if(itr == 0)
-                cnt++;
+            if(nums[i] == 0)
+            {    
+                cnt++, idx = i;
+                continue;
+            }
+            p *= nums[i];
         }
         if(cnt > 1)
             return ans;
+        if(cnt == 1)
+        {
+            ans[idx] = p;
+            return ans;
+        }
         ans[n-1] = 1;
         for(int i = n-2; i >= 0; i--)
         {
