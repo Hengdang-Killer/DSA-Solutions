09c522788df8167014efc51123181598688164e0
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-03-23 18:40:38 +0530

    Time: 5 ms (97.67%), Space: 11.5 MB (13.32%) - LeetHub

--- 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
@@ -6,13 +6,13 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        long long cnt = 0;
+        int cnt = 0;
         for(char &itr : s)
         {
             if(itr == c)
                 cnt++;
         }
-        long long ans = (cnt*(cnt+1))/2;
+        long long ans = ((long long)cnt*(cnt+1))/2;
         return ans;
     }
 };
