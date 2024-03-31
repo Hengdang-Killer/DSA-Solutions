3b34b4319cb21cf118ae52241ae54ba2b972ea94
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-03-31 20:48:29 +0530

    Time: 1306 ms (100.00%), Space: 626.1 MB (100.00%) - LeetHub

--- 3098-find-the-sum-of-subsequence-powers/3098-find-the-sum-of-subsequence-powers.cpp
+++ 3098-find-the-sum-of-subsequence-powers/3098-find-the-sum-of-subsequence-powers.cpp
@@ -1,69 +1,63 @@
 const int mod = 1e9+7;
 const int N = 50;
-
 int dp[N+1][N+1][N+1][1230];
 set<int> st;
 map<int, int> mp1, mp2;
-class Solution {
-public:
-    int sumOfPowers(vector<int>& arr, int k) {
-        int n = arr.size();
-        
-        sort(arr.begin(), arr.end());
-        st.clear();
-        mp1.clear();
-        mp2.clear();
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                st.insert(arr[j] - arr[i]);
-            }
+
+class Solution 
+{
+private:
+    int n, idx;
+    int recursion(int i, int prev, int len, int mn, int &k, vector<int> &nums)
+    {
+        if(len == k)
+        {
+            if(mn != idx)
+                return mp2[mn];
+            return 0;
         }
-        int idx = 0;
-        for(int x: st){
+        if(i >= n)
+            return 0;
+        if(dp[i][prev][len][mn] != -1)
+            return dp[i][prev][len][mn];
+        int ans = recursion(i+1, prev, len, mn, k, nums);    
+        if(len == 0)
+            ans += recursion(i+1, i, len+1, idx, k, nums);
+        else
+        {
+            int pv = nums[prev], cv = nums[i], diff = cv - pv;
+            if(diff < mp2[mn])
+                ans += recursion(i+1, i, len+1, mp1[diff], k, nums);
+            else
+                    ans += recursion(i+1, i, len+1, mn, k, nums);
+        }
+        ans %= mod;    
+        return dp[i][prev][len][mn] = ans;
+    }
+public:
+    int sumOfPowers(vector<int>& nums, int k) 
+    {
+        n = nums.size();
+        sort(nums.begin(), nums.end());
+        st.clear(), mp1.clear(), mp2.clear();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i+1; j < n; j++)
+                st.insert(nums[j] - nums[i]);
+        }
+        idx = 0;
+        for(int x: st)
+        {
             mp1[x] = idx;
             mp2[idx++] = x;
         }
         mp1[1e9] = idx;
         mp2[idx] = 1e9;
-        
-        
-        auto rec = [&](int i, int prv, int cnt, int mn, auto &&rec)->int{
-            if(cnt == k){
-                if(mn != idx) return mp2[mn];
-                return 0;
-            }
-            if(i == n)
-                return 0;
-            
-            int &ans = dp[i][prv+1][cnt][mn];
-            
-            if(ans != -1) return dp[i][prv+1][cnt][mn];
-            
-            ans = rec(i+1, prv, cnt, mn, rec);
-            
-            if(prv == -1){
-                ans += rec(i+1, i, 1, idx, rec);
-            }else{
-                int pv = arr[prv];
-                int cv = arr[i];
-                int diff = cv - pv;
-                if(diff < mp2[mn]){
-                    ans += rec(i+1, i, cnt+1, mp1[diff], rec);
-                }else{
-                    ans += rec(i+1, i, cnt+1, mn, rec);
-                }
-            }
-            ans %= mod;
-            
-            return ans;
-        };
- 
         for(int i = 0; i<=n; i++)
             for(int j = 0; j<=n; j++)
                 for(int jk = 0; jk<=k; jk++)
                     for(int ik = 0; ik<=idx+1; ik++)
                         dp[i][j][jk][ik] = -1;
-
-        return rec(0, -1, 0, idx, rec);
+        return recursion(0, 0, 0, idx, k, nums);
     }
 };
