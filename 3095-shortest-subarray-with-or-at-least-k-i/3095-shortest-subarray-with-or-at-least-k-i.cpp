0e9ca8a04166b68eb4dc2523b9126482c3014af7
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-03-31 19:38:43 +0530

    Time: 4 ms (83.33%), Space: 26.1 MB (16.67%) - LeetHub

---
+++ 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.cpp
@@ -0,0 +1,40 @@
+class Solution 
+{
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) 
+    {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int n = nums.size();
+        vector<int> mp(32, 0);
+        int ans = n + 1;
+        for(int l = 0, r = 0; r < n; r++)
+        {
+            int val = 0;
+            for(int i = 0; i < 32; i++)
+            {    
+                if((nums[r]>>i & 1)) 
+                    mp[i]++;
+                if(mp[i])
+                    val |= 1 << i;
+            }
+            while(val >= k && l <= r)
+            {
+                ans = min(ans, r - l + 1);
+                val = 0;
+                for(int i = 0; i < 32; i++)
+                {
+                    if((nums[l]>>i & 1))
+                        mp[i]--;
+                    if(mp[i])
+                        val |= 1<<i;
+                }
+                l++;
+            }
+        }
+        if(ans > n)
+            ans = -1;
+        return ans;
+    }
+};
