91bac36b2aeb3d9a640f61761277649f0762e57d
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-01 13:56:20 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (57.50%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int lengthOfLastWord(string s) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int prev = 0, cur = 0;
         for(char &itr : s)
         {
