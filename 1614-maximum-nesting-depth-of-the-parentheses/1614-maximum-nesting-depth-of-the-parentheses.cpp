dd62a092b16198ffaebed274ff41b3561dea9046
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-04 20:36:59 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (14.40%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    int maxDepth(string s) 
+    {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int ans = 0, cur = 0;
+        for (char ch : s) 
+        {
+            if (ch == '(') 
+            {
+                cur++;
+                if (ans < cur)
+                    ans = cur;
+            } 
+            else if (ch == ')') 
+                cur--;
+        }
+        return ans;   
+    }
+};
