b9809fa1d725090bff5cbd053978b3ce3b3e4b26
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-07 23:24:40 +0530

    Time: 2 ms (47.10%), Space: 9.3 MB (9.43%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{
+private:
+    int recursion(int i, int brkt, string &s, vector<vector<int>> &dp)
+    {
+        if(i >= s.size())
+        {
+            if(brkt == 0)
+                return 1;
+            return 0;
+        }
+        if(dp[i][brkt] != -1)
+            return dp[i][brkt];
+        if(s[i] == '(')
+            return dp[i][brkt] = recursion(i+1, brkt + 1, s, dp);
+        if(s[i] == ')')
+        {
+            if(brkt > 0)
+                return dp[i][brkt] = recursion(i+1, brkt-1, s, dp); 
+            return dp[i][brkt] = 0;
+        }
+        int notTake = 0, open = 0, close = 0;
+        if(brkt != 0)
+            close = recursion(i+1, brkt - 1, s, dp);
+        open = recursion(i+1, brkt+1, s, dp);
+        notTake = recursion(i+1, brkt, s, dp);
+        return dp[i][brkt] = notTake | open | close;
+    }
+public:
+    bool checkValidString(string s) 
+    {
+        vector<vector<int>> dp(s.size(), vector<int> (s.size() + 1, -1));
+        return recursion(0, 0, s, dp);
+    }
+};
