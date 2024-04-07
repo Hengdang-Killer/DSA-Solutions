e38dd11309c45f7cd0da449a2f1116ad5d0b6197
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-07 23:32:39 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (92.84%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,38 +1,27 @@
 class Solution 
 {
-private:
-    int recursion(int i, int brkt, string &s, vector<vector<int>> &dp)
-    {
-        if(i >= s.size())
-        {
-            if(brkt == 0)
-                return 1;
-            return 0;
-        }
-        if(dp[i][brkt] != -1)
-            return dp[i][brkt];
-        if(s[i] == '(')
-            return dp[i][brkt] = recursion(i+1, brkt + 1, s, dp);
-        if(s[i] == ')')
-        {
-            if(brkt > 0)
-                return dp[i][brkt] = recursion(i+1, brkt-1, s, dp); 
-            return dp[i][brkt] = 0;
-        }
-        int notTake = 0, open = 0, close = 0;
-        if(brkt != 0)
-            close = recursion(i+1, brkt - 1, s, dp);
-        open = recursion(i+1, brkt+1, s, dp);
-        notTake = recursion(i+1, brkt, s, dp);
-        return dp[i][brkt] = notTake | open | close;
-    }
 public:
     bool checkValidString(string s) 
     {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-        vector<vector<int>> dp(s.size(), vector<int> (s.size() + 1, -1));
-        return recursion(0, 0, s, dp);
+        int openMin = 0, openMax = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '(')
+                openMin++, openMax++;
+            else if(s[i] == ')')
+                openMin--, openMax--;
+            else
+                openMin--, openMax++;
+            if(openMax < 0)
+                return false;
+            if(openMin < 0)
+                openMin = 0;
+        }
+        if(openMin == 0)
+            return true;
+        return false;
     }
 };
