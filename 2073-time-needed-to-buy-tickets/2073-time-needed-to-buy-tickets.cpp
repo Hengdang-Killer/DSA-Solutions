115dc996e3ef1294bbb9992732ce3c7803459001
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-09 19:05:21 +0530

    Time: 4 ms (41.19%), Space: 9.3 MB (91.31%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -4,17 +4,13 @@ public:
     int timeRequiredToBuy(vector<int> &tickets, int k) 
     {
         int ans = 0;
-        for(;;)
+        for (int i = 0; i < tickets.size(); ++i) 
         {
-            for(int i = 0; i < tickets.size(); i++)
-            {
-                if(tickets[i] == 0)
-                    continue;
-                tickets[i]--, ans++;
-                if(i == k && tickets[i] == 0)
-                    return ans;
-            }
+            if (i <= k) 
+                ans += min(tickets[i], tickets[k]);
+            else 
+                ans += min(tickets[i], tickets[k] - 1);
         }
-        return -1;
+        return ans;
     }
 };
