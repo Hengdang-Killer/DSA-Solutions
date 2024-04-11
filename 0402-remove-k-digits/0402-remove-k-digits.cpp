650add8f34fdcd998f60ea3414c31cdddde6bc43
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-11 12:28:21 +0530

    Time: 5 ms (95.06%), Space: 9.7 MB (86.82%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     string removeKdigits(string &num, int k) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if(k >= num.size())
             return "0";
         stack<char> stk;        
