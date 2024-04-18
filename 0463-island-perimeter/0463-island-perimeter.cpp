c720d3435f6db36a44f19d1e98939476f68cd335
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-18 09:52:11 +0530

    Time: 74 ms (57.34%), Space: 108.9 MB (6.53%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -7,25 +7,55 @@ public:
         cin.tie(0);
         cout.tie(0);
         int n = grid.size(), m = grid[0].size();
-        int ans = 0;
+        bool ok = 0;
+        queue<pair<int, int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
                 if(grid[i][j] == 1)
                 {
-                    int cnt = 0;
-                    if(i-1 >= 0 && grid[i-1][j] == 1)
-                        cnt++;
-                    if(i+1 < n && grid[i+1][j] == 1)
-                        cnt++;
-                    if(j-1 >= 0 && grid[i][j-1] == 1)
-                        cnt++;
-                    if(j+1 < m && grid[i][j+1] == 1)
-                        cnt++;
-                    ans += 4 - cnt;
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                    ok = 1;
+                    break;
                 }
             }
+            if(ok)
+                break;
+        }
+        int ans = 0;
+        while(!q.empty())
+        {
+            int i = q.front().first, j = q.front().second;
+            q.pop();
+            int cnt = 0;
+            if(i-1 >= 0 && grid[i-1][j] == 1)
+            {
+                cnt++;
+                if(!vis[i-1][j])
+                    q.push({i-1, j}), vis[i-1][j] = 1;
+            }
+            if(i+1 < n && grid[i+1][j] == 1)
+            {
+                cnt++;
+                if(!vis[i+1][j])
+                    q.push({i+1, j}), vis[i+1][j] = 1;
+            }
+            if(j-1 >= 0 && grid[i][j-1] == 1)
+            {
+                cnt++;
+                if(!vis[i][j-1])
+                    q.push({i, j-1}), vis[i][j-1] = 1;
+            }
+            if(j+1 < m && grid[i][j+1] == 1)
+            {            
+                cnt++;
+                if(!vis[i][j+1])
+                    q.push({i, j+1}), vis[i][j+1] = 1;
+            }
+            ans += 4 - cnt;
         }
         return ans;
     }
