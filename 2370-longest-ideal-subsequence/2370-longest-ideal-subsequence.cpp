94f2016d0c00ebc47126273b2ff8553f8e7c5175
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-25 10:38:19 +0530

    Time: 95 ms (52.10%), Space: 15.2 MB (49.58%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -0,0 +1,28 @@
+class Solution 
+{
+public:
+    int longestIdealString(string s, int k) 
+    {
+        if(k == 25)
+            return s.size();
+        int n = s.size();
+        vector<int> v(26, -1);
+        vector<int> dp(n);
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            int val = 0;
+            for(int j = 0; j < 26; j++)
+            {
+                if(v[j] >= 0 && abs(j - (s[i]-'a')) <= k)
+                {
+                    val = max(val, dp[v[j]]);
+                }
+            }
+            v[s[i]-'a'] = i;
+            dp[i] = val + 1;
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
