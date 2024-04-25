af0b294906e39bb94f79a81a1500d6c31cca380f
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-25 10:38:43 +0530

    Time: 79 ms (52.52%), Space: 15.1 MB (50.42%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int longestIdealString(string s, int k) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         if(k == 25)
             return s.size();
         int n = s.size();
