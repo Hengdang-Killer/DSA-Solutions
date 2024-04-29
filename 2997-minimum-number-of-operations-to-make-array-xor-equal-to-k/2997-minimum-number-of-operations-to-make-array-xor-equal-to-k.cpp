6e62fea6b1c209560bf4bd090522314c2993ad25
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-29 22:19:49 +0530

    Time: 55 ms (98.78%), Space: 91.8 MB (8.20%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int minOperations(vector<int>& nums, int k) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int finalXor = 0;
         for (int &n : nums) 
         {
