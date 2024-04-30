371e721d68511bb2625fb6d4b3b6c362756a6f78
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-30 22:12:00 +0530

    Time: 378 ms (44.91%), Space: 20.1 MB (25.15%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     long long wonderfulSubstrings(string word) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         unordered_map<int, int> count;
         count[0] = 1;
         long long result = 0;
