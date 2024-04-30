156515709fb902eebe0aaa59ae5862b3ebd2d2f2
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-04-30 22:11:30 +0530

    Time: 438 ms (20.36%), Space: 19.9 MB (31.14%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -1,21 +1,23 @@
-class Solution {
+class Solution 
+{
 public:
-    long long wonderfulSubstrings(string word) {
+    long long wonderfulSubstrings(string word) 
+    {
         unordered_map<int, int> count;
         count[0] = 1;
         long long result = 0;
         int bitmask = 0;
-
-        for (char ch : word) {
+        for (char &ch : word) 
+        {
             int charIndex = ch - 'a';
             bitmask ^= 1 << charIndex;
             result += count[bitmask];
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10; i++) 
+            {
                 result += count[bitmask ^ (1 << i)];
             }
             count[bitmask]++;
         }
-
         return result;
     }
 };
