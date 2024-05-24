a1d22ba5f4c8e38200c2eb55db6c8691f393605b
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-05-24 09:33:57 +0530

    Time: 20 ms (34.56%), Space: 22.6 MB (35.92%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,14 +1,13 @@
 class Solution 
 {
 private:
-    map<int, map<string, int>> dp;
-    int recursion(int i, string j, vector<string>& words, vector<char>& letters, vector<int>& score)
+    int recursion(int i, string j, vector<string>& words, vector<char>& letters, vector<int>& score, map<int, map<string, int>> &dp)
     {
         if(i >= words.size())
             return 0;
         if(dp[i].find(j) != dp[i].end())
             return dp[i][j];
-        int notTake = recursion(i+1, j, words, letters, score);
+        int notTake = recursion(i+1, j, words, letters, score, dp);
         int take = 0, itr = 0;
         string temp = j;
         while(itr < words[i].size())
@@ -27,32 +26,23 @@ private:
             itr++;
         }
         if(itr == words[i].size())
-            take += recursion(i+1, temp, words, letters, score);
+            take += recursion(i+1, temp, words, letters, score, dp);
         else
-            take = recursion(i+1, j, words, letters, score);
+            take = recursion(i+1, j, words, letters, score, dp);
         return dp[i][j] = max(take, notTake);
     }
 public:
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) 
     {
-        dp.clear();
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        map<int, map<string, int>> dp;
         string bitmask = "";
         for(int i = 0; i < letters.size(); i++)
         {
             bitmask.push_back('0');
         }
-        int ans = recursion(0, bitmask, words, letters, score);
-        /*
-        for(auto itr : dp)
-        {
-            cout << itr.first << ":-\n";
-            for(auto itr2 : itr.second)
-            {
-                cout << itr2.first << " " << itr2.second << '\n';
-            }
-            cout<<'\n';
-        }
-        */
-        return ans;
+        return recursion(0, bitmask, words, letters, score, dp);
     }
 };
