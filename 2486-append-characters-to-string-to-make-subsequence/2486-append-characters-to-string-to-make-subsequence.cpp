e26ae20cc28525638599e35db6657d7fb5c3dd98
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-03 11:36:03 +0530

    Time: 13 ms (92.57%), Space: 15.9 MB (5.18%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -3,15 +3,18 @@ class Solution
 public:
     int appendCharacters(string &s, string &t) 
     {
-        vector<set<int>> pos(26);
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        vector<vector<int>> pos(26);
         for(int i = 0; i < s.size(); i++)
         {
-            pos[s[i]-'a'].insert(i);
+            pos[s[i]-'a'].push_back(i);
         }
         int ans = 0, lastPos = -1;
         for(int i = 0; i < t.size(); i++)
         {
-            auto itr = pos[t[i]-'a'].upper_bound(lastPos);
+            auto itr = upper_bound(pos[t[i]-'a'].begin(), pos[t[i]-'a'].end(), lastPos);
             if(itr == pos[t[i]-'a'].end())
             {
                 ans = t.size() - i;
