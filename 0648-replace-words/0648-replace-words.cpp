26ddafda6d203b9de90d61ae283fbb7a36d1f670
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-07 11:55:28 +0530

    Time: 90 ms (58.72%), Space: 66.9 MB (80.11%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,74 @@
+class Trie
+{
+public:
+    bool isEnd;
+    Trie *nxt[26];
+    Trie() 
+    {
+        isEnd = 0;
+        for(int i = 0; i < 26; i++)
+            nxt[i] = NULL;
+    }
+};
+
+class Solution 
+{
+private:
+    Trie *root = new Trie();
+    void insert(string &s)
+    {
+        int n = s.size();
+        Trie *itr = root;
+        for(int i = 0; i < n; i++)
+        {
+            if(!itr->nxt[s[i] - 'a'])
+                itr->nxt[s[i]-'a'] = new Trie();
+            itr = itr->nxt[s[i] - 'a'];
+        }
+        itr->isEnd = 1;
+    }
+    void search(string &s, string &ans)
+    {
+        int n = s.size();
+        Trie *itr = root;
+        for(int i = 0; i < n; i++)
+        {
+            if(!itr->nxt[s[i] - 'a'])
+            {
+                if(itr->isEnd)
+                    return;
+                while(i < n)
+                {
+                    ans.push_back(s[i]);
+                    i++;
+                }
+                return;
+            }
+            ans.push_back(s[i]);
+            itr = itr->nxt[s[i] - 'a'];
+            if(itr->isEnd)
+                return;
+        }
+        return;
+    }
+public:
+    string replaceWords(vector<string>& dictionary, string &s) 
+    {
+        for(auto &itr : dictionary)
+        {
+            insert(itr);
+        }
+        string ans = "", temp = "";
+        int i = 0, n = s.size();
+        while(i < n)
+        {
+            if(s[i] == ' ')
+                search(temp, ans), ans.push_back(' '), temp = "";
+            else
+                temp.push_back(s[i]);
+            i++;
+        }
+        search(temp, ans);
+        return ans;
+    }
+};
