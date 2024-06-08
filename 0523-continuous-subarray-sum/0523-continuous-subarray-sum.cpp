223704becbfa36bc18c30b6e0b30fee21043236c
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-08 12:33:30 +0530

    Time: 125 ms (98.30%), Space: 134.7 MB (31.96%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     bool checkSubarraySum(vector<int>& nums, int k) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         unordered_set<int> st;
         int sum = 0, prevSum;
         for(auto &itr : nums)
