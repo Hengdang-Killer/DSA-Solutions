4c9ed9f34a31437c990efc3a9ab5b67a05362b71
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-10 11:31:22 +0530

    Time: 5 ms (31.17%), Space: 10.3 MB (10.73%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int heightChecker(vector<int>& heights) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<int> freq(101, 0);
         for(auto &itr : heights)
             freq[itr]++;
