e10c6c119563c7a95a191aa6be409e90db2e6ae8
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-11 11:33:01 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (71.58%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<int> freq(1001, 0);
         for(auto itr : arr1)
             freq[itr]++;
