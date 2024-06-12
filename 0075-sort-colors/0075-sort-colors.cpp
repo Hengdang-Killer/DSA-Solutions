00b2d5bbdb45edbae25b4f7c52bdb71bec95ac67
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-12 10:58:23 +0530

    Time: 5 ms (17.05%), Space: 9.8 MB (71.79%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     void sortColors(vector<int>& nums) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int r = nums.size() - 1, l = 0, mid = 0;
         while(mid <= r)
         {
