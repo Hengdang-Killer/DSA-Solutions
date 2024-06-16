8aa3a4e7b7a455745d5de6f79ea49338cb6c61c5
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-16 12:30:03 +0530

    Time: 8 ms (22.13%), Space: 13.8 MB (12.30%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -3,9 +3,6 @@ class Solution
 public:
     int minPatches(vector<int>& nums, int n) 
     {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         long long miss = 1;
         int ans = 0, i = 0;
         while (miss <= n) 
