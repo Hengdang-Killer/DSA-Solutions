8a2cbcca6c2839a683dfa6929fcff7f466acc253
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-19 11:44:15 +0530

    Time: 205 ms (33.13%), Space: 172.3 MB (45.78%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,27 @@
+// grow time can overlap
+// plant time cannot overlap
+// so that means most overlapping growth would give us minimum time
+
+class Solution 
+{
+private:
+    static bool comp(pair<int, int> &a, pair<int, int> &b)
+    {
+        return a.first > b.first;
+    }
+public:
+    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) 
+    {
+        vector<pair<int, int>> v;
+        for(int i = 0; i < plantTime.size(); i++)
+            v.push_back({growTime[i] + 1, plantTime[i]});
+        sort(v.begin(), v.end(), comp);
+        int prev = 0, ans = 0;
+        for(int i = 0; i < plantTime.size(); i++)
+        {
+            ans = max(ans, prev + v[i].first + v[i].second);
+            prev += v[i].second;
+        }
+        return ans - 1;
+    }
+};
