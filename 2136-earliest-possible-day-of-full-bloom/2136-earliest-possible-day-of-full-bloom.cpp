8929d32882fa9079cd1024c71b4ab55856de38ff
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-19 11:44:59 +0530

    Time: 125 ms (98.80%), Space: 171.9 MB (62.95%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -12,6 +12,9 @@ private:
 public:
     int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<pair<int, int>> v;
         for(int i = 0; i < plantTime.size(); i++)
             v.push_back({growTime[i] + 1, plantTime[i]});
