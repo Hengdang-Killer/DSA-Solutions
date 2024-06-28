03ef0a54ae9076002a06171eda50bccf1a27f51e
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-29 01:12:45 +0530

    Time: 83 ms (93.39%), Space: 76 MB (94.06%) - LeetHub

--- 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
@@ -2,23 +2,15 @@ class Solution
 {
 public:
     long long maximumTotalCost(vector<int>& nums) 
-    {
-        long long ans = 0;    
+    { 
         int n = nums.size();
-        vector<vector<long long>> dp(n+1, vector<long long> (2, 0));
+        long long prev0 = 0, prev1 = 0;
         for(int i = n-1; i >= 0; i--)
         {
-            for(int j = 0; j < 2; j++)
-            {
-                long long newSubArray = nums[i] + dp[i+1][1];
-                long long oldSubArray = 0;
-                if(j == 1)
-                    oldSubArray = -nums[i] + dp[i+1][0];
-                else
-                    oldSubArray = nums[i] + dp[i+1][1];
-                dp[i][j] = max(newSubArray, oldSubArray);
-            }
+            long long cur1 = max(-nums[i] + prev0, nums[i] + prev1);
+            long long cur0 = max(nums[i] + prev1, nums[i] + prev1);
+            prev0 = cur0, prev1 = cur1;
         }
-        return dp[0][0];
+        return prev0;
     }
 };
