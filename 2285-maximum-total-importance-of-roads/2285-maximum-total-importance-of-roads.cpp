34ee01491b4d922b43bfdeeb721cda6a773110ec
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-06-29 00:46:28 +0530

    Time: 256 ms (78.45%), Space: 119.8 MB (72.59%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -4,17 +4,12 @@ public:
     long long maximumImportance(int n, vector<vector<int>>& roads) 
     {
         long long ans = 0;
-        vector<pair<int, int>> degree(n);
-        vector<int> numbering(n);
-        for(int i = 0; i < n; i++)
-            degree[i].second = i, degree[i].first = 0;
+        vector<int> degree(n, 0);
         for(auto &itr : roads)
-            degree[itr[0]].first++, degree[itr[1]].first++;
+            degree[itr[0]]++, degree[itr[1]]++;
         sort(degree.rbegin(), degree.rend());
         for(auto &itr : degree)
-            numbering[itr.second] = n--;
-        for(auto &itr : roads)
-            ans += numbering[itr[0]] + numbering[itr[1]];
+            ans += (long long)(n--)*itr;
         return ans;
     }
 };
