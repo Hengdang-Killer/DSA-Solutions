8300c8e14897692b6fb293bf53a35d695d6b5b7f
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-07-04 11:43:55 +0530

    Time: 458 ms (39.76%), Space: 256.5 MB (96.69%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,39 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution 
-{
+class Solution {
 public:
-    ListNode* mergeNodes(ListNode* head) 
-    {
-        head = head->next;
-        ListNode* root = new ListNode();
-        ListNode* ans = root;
-        int sum = 0;
-        while(head != nullptr)
-        {
-            if(head->val == 0)
-            {
-                root->val = sum;
-                sum = 0;
-                if(head->next)
-                {
-                    ListNode* temp = new ListNode();
-                    root->next = temp;
-                    root = root->next;
-                }
+    ListNode* mergeNodes(ListNode* head) {
+        // Initialize a sentinel/dummy node with the first non-zero value.
+        ListNode* modify = head->next;
+        ListNode* nextSum = modify;
+
+        while (nextSum) {
+            int sum = 0;
+            // Find the sum of all nodes until you encounter a 0.
+            while (nextSum->val != 0) {
+                sum = sum + nextSum->val;
+                nextSum = nextSum->next;
             }
-            else
-                sum += head->val;
-            head = head->next;
+
+            // Assign the sum to the current node's value.
+            modify->val = sum;
+            // Move nextSum to the first non-zero value of the next block.
+            nextSum = nextSum->next;
+            // Move modify also to this node.
+            modify->next = nextSum;
+            modify = modify->next;
         }
-        return ans;
+        return head->next;
     }
 };
