1fb08bb51191bfa35ceabb23367ce466a66a183a
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-07-10 22:03:20 +0530

    Time: 8 ms (24.07%), Space: 14 MB (41.82%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int minOperations(vector<string>& logs) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int ans = 0;
         for(auto &itr : logs)
         {
