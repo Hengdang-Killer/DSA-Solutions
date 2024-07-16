010f76c533d0611ed55873c91ff7ed59e7148bd8
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-07-17 00:44:21 +0530

    Time: 467 ms (96.12%), Space: 239.8 MB (63.69%) - LeetHub

--- 3187-peaks-in-array/3187-peaks-in-array.cpp
+++ 3187-peaks-in-array/3187-peaks-in-array.cpp
@@ -1,8 +1,3 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
-
 struct segment
 {
     int n;
@@ -66,8 +61,6 @@ struct segment
         update(0, n - 1, 0, idx, v);
     }
 };
-
-
 
 class Solution 
 {
