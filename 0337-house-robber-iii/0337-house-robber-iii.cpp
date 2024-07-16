0964eeb40055199e8b82b40a93aafab6a186ed1e
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-07-16 11:35:25 +0530

    Time: 22 ms (16.16%), Space: 25.3 MB (25.66%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+private:
+    int recursion(TreeNode* cur, unordered_map<TreeNode*, int> &dp)
+    {
+        if(dp.find(cur) != dp.end())
+            return dp[cur];
+        int notTake = 0;
+        if(cur->left)
+            notTake += recursion(cur->left, dp);
+        if(cur->right)
+            notTake += recursion(cur->right, dp);
+        int take = cur->val;
+        if(cur->left && cur->left->left)
+            take += recursion(cur->left->left, dp);
+        if(cur->left && cur->left->right)
+            take += recursion(cur->left->right, dp);
+        if(cur->right && cur->right->left)
+            take += recursion(cur->right->left, dp);
+        if(cur->right && cur->right->right)
+            take += recursion(cur->right->right, dp);
+        return dp[cur] = max(take, notTake);
+    }
+public:
+    int rob(TreeNode* root) 
+    {
+        unordered_map<TreeNode*, int> dp;
+        return recursion(root, dp);
+    }
+};
