2eba5e2949fa9a33a5014f640f40eba03fd6111e
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-07-28 11:38:28 +0530

    Time: 416 ms (85.11%), Space: 180.5 MB (94.29%) - LeetHub

---
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
@@ -0,0 +1,36 @@
+class Solution 
+{
+public:
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) 
+    {
+        vector<int> adj[n+1];
+        for(auto &itr : edges)
+            adj[itr[0]].push_back(itr[1]), adj[itr[1]].push_back(itr[0]);
+        queue<pair<int, int>> q;
+        vector<int> dist1(n+1, -1), dist2(n+1, -1);
+        q.push({1, 1});
+        dist1[1] = 0;
+        while(!q.empty())
+        {
+            int node = q.front().first, type = q.front().second;
+            q.pop();
+            int timeTaken = (type == 1) ? dist1[node] : dist2[node];
+            if((timeTaken / change) % 2) // red light
+                timeTaken = change * (timeTaken / change + 1) + time;
+            else // green light
+                timeTaken += time;
+            for(auto itr : adj[node])
+            {
+                if(dist1[itr] == -1)
+                    dist1[itr] = timeTaken, q.push({itr, 1});
+                else if(dist2[itr] == -1 && dist1[itr] != timeTaken)
+                {
+                    if(itr == n)
+                        return timeTaken;
+                    dist2[itr] = timeTaken, q.push({itr, 2});
+                }
+            }
+        }
+        return 0;
+    }
+};
