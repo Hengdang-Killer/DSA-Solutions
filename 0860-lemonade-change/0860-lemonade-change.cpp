57d6362bb3245a09000e8e35cde975dffaf6f3ce
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-08-15 14:56:14 +0530

    Time: 49 ms (98.81%), Space: 86 MB (32.71%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -3,27 +3,29 @@ class Solution
 public:
     bool lemonadeChange(vector<int>& bills) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int cnt1 = 0, cnt2 = 0;
         for(auto &itr : bills)
         {
             if(itr == 5)
                 cnt1++;
-            else if(itr == 10)
-            {
-                if(cnt1 == 0)
-                    return false;
-                cnt1--, cnt2++;
-            }
             else
             {
                 if(cnt1 == 0)
                     return false;
-                if(cnt2 == 0 && cnt1 < 3)
-                    return false;
-                if(cnt2 == 0)
-                    cnt1 -= 3;
+                if(itr == 10)
+                    cnt1--, cnt2++;
                 else
-                    cnt1--, cnt2--;
+                {
+                    if(cnt2 == 0 && cnt1 < 3)
+                        return false;
+                    if(cnt2 == 0)
+                        cnt1 -= 3;
+                    else
+                        cnt1--, cnt2--;
+                }
             }
         }
         return true;    
