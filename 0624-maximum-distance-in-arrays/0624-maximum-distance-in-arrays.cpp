b0e70af312edff5fea97921f3d635a0e604339a9
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-08-17 01:16:37 +0530

    Time: 185 ms (99.53%), Space: 108.1 MB (25.94%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int maxDistance(vector<vector<int>>& arrays) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int mn = arrays[0][0], mx = arrays[0].back();
         int ans = 0;
         for (int i = 1; i < arrays.size(); i++) 
