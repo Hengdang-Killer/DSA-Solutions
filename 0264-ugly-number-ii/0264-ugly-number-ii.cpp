2e23b3cc9808aba09170aaa251b4fe90f12962cd
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-08-18 14:11:12 +0530

    Time: 7 ms (51.38%), Space: 9.7 MB (72.38%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -3,23 +3,21 @@ class Solution
 public:
     int nthUglyNumber(int n) 
     {
-        priority_queue<long, vector<long>, greater<long>> pq;
-        unordered_set<long> st;
-        vector<int> primeFactors = {2, 3, 5};
-        pq.push(1);
-        st.insert(1);
-        long cur = 1;
-        for (int i = 0; i < n; i++) 
+        vector<int> dp(n);
+        dp[0] = 1;
+        int idxOf2 = 0, idxOf3 = 0, idxOf5 = 0;
+        int nxtOf2 = 2, nxtOf3 = 3, nxtOf5 = 5;
+        for (int i = 1; i < n; i++) 
         {
-            cur = pq.top();
-            pq.pop();
-            for (int prime : primeFactors) 
-            {
-                long nxt = cur * prime;
-                if (st.find(nxt) == st.end()) 
-                    pq.push(nxt), st.insert(nxt);
-            }
+            int nxt = min(nxtOf2, min(nxtOf3, nxtOf5));
+            dp[i] = nxt;
+            if (nxt == nxtOf2) 
+                nxtOf2 = dp[++idxOf2] * 2;
+            if (nxt == nxtOf3)
+                nxtOf3 = dp[++idxOf3] * 3;
+            if (nxt == nxtOf5)
+                nxtOf5 = dp[++idxOf5] * 5;
         }
-        return (int)cur;     
+        return dp[n - 1];
     }
 };
