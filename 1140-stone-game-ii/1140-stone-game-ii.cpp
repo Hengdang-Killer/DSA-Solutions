53a7e290e657c4f2d2a42d56ec766a1419bec7a9
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-08-20 18:57:32 +0530

    Time: 13 ms (73.54%), Space: 12.4 MB (58.99%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,23 +1,5 @@
 class Solution 
 {
-private:
-    int recursion(int i, int j, int n, vector<int> &suf, vector<int> &piles, vector<vector<int>> &dp)
-    {
-        if(i >= n)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int val = 0, sum = 0;
-        for(int k = 1; k <= 2*j; k++)
-        {
-            if(i + k - 1 >= n)
-                break;
-            sum += piles[i + k - 1];
-            int nxtM = max(k, j);
-            val = max(val, suf[i] - recursion(i + k, nxtM, n, suf, piles, dp));
-        }
-        return dp[i][j] = val;
-    }
 public:
     int stoneGameII(vector<int>& piles) 
     {
@@ -25,14 +7,29 @@ public:
         cin.tie(0);
         cout.tie(0);
         int n = piles.size();
+        vector<vector<int>> dp(n, vector<int> (n+1, 0));
         vector<int> suf(n, 0);
         for(int i = n-1; i >= 0; i--)
         {
+            suf[i] = piles[i];
             if(i != n-1)
                 suf[i] += suf[i+1];
-            suf[i] += piles[i];
         }
-        vector<vector<int>> dp(n, vector<int> (n+1, -1));
-        return recursion(0, 1, n, suf, piles, dp);
+        for(int i = n-1; i >= 0; i--)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                for(int k = 2*j; k >= 1; k--)
+                {
+                    if(i + k >= n)
+                    {
+                        dp[i][j] = suf[i];
+                        break;
+                    }
+                    dp[i][j] = max(dp[i][j], suf[i] - dp[i+k][max(k, j)]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
