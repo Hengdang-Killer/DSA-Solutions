61c21519c3038f7212d81bb75f22f3aac05d1464
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-09-02 17:45:36 +0530

    Time: 40 ms (99.08%), Space: 77.2 MB (8.71%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int chalkReplacer(vector<int>& chalk, int k) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int n = chalk.size();
         long sum = 0;
         for(auto &itr : chalk)
