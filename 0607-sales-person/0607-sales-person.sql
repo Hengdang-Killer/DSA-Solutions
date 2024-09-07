8ebc59b7ca2054b76a7c85d9910e5ba660031128
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-09-07 23:23:03 +0530

    Time: 1192 ms (83.23%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -4,11 +4,12 @@ SELECT
 FROM 
     Salesperson s 
 WHERE 
-    s.name NOT IN (
-        SELECT s.name 
-        FROM Salesperson s 
-        JOIN 
-            Orders o ON s.sales_id = o.sales_id 
-        LEFT JOIN 
-            Company c ON c.com_id = o.com_id WHERE c.name="RED"
-);
+    NOT EXISTS (
+        SELECT 1
+        FROM 
+            Orders o 
+            LEFT JOIN Company c ON c.com_id = o.com_id
+        WHERE 
+            s.sales_id = o.sales_id 
+            AND c.name = 'RED'
+    );
