c80a095728294d9f66af47d8112abc44bd15d392
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-09-15 19:30:24 +0530

    Time: 15 ms (97.21%), Space: 17.7 MB (75.12%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     int findTheLongestSubstring(string s) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int preXor = 0;
         int mp[26] = {0};
         mp['a' - 'a'] = 1, mp['e' - 'a'] = 2, mp['i' - 'a'] = 4, mp['o' - 'a'] = 8, mp['u' - 'a'] = 16;
