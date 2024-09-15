7274827e02d400575705ac137b3c4978ed899cac
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-09-15 19:29:56 +0530

    Time: 48 ms (65.12%), Space: 17.7 MB (75.12%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+public:
+    int findTheLongestSubstring(string s) 
+    {
+        int preXor = 0;
+        int mp[26] = {0};
+        mp['a' - 'a'] = 1, mp['e' - 'a'] = 2, mp['i' - 'a'] = 4, mp['o' - 'a'] = 8, mp['u' - 'a'] = 16;
+        vector<int> idx(32, -1);
+        int ans = 0, n = s.size();
+        for (int i = 0; i < n; i++) 
+        {
+            preXor ^= mp[s[i] - 'a'];
+            if (idx[preXor] == -1 && preXor != 0) 
+                idx[preXor] = i;
+            ans = max(ans, i - idx[preXor]);
+        }
+        return ans;
+    }
+};
