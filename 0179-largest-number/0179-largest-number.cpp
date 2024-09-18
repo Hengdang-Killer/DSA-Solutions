39c4b0183d68d821670a626c5ae10bf5a7dae795
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-09-18 19:39:51 +0530

    Time: 7 ms (61.31%), Space: 17.4 MB (36.02%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,42 +1,26 @@
 class Solution 
 {
-private:
-    bool compare(int &firstNum, int &secondNum) 
-    {
-        return to_string(firstNum) + to_string(secondNum) > to_string(secondNum) + to_string(firstNum);
-    }
-    vector<int> merge(vector<int>& leftHalf, vector<int>& rightHalf) 
-    {
-        vector<int> sortedNums;
-        int leftIndex = 0, rightIndex = 0;
-        while (leftIndex < leftHalf.size() && rightIndex < rightHalf.size()) 
-        {
-            if (compare(leftHalf[leftIndex], rightHalf[rightIndex]))
-                sortedNums.push_back(leftHalf[leftIndex++]);
-            else 
-                sortedNums.push_back(rightHalf[rightIndex++]);
-        }
-        while (leftIndex < leftHalf.size())
-            sortedNums.push_back(leftHalf[leftIndex++]);
-        while (rightIndex < rightHalf.size())
-            sortedNums.push_back(rightHalf[rightIndex++]);
-        return sortedNums;
-    }
-    vector<int> mergeSort(vector<int>& nums, int left, int right) 
-    {
-        if (left >= right) 
-            return {nums[left]};
-        int mid = left + (right - left) / 2;
-        vector<int> leftHalf = mergeSort(nums, left, mid);
-        vector<int> rightHalf = mergeSort(nums, mid + 1, right);
-        return merge(leftHalf, rightHalf);
-    }
 public:
-    string largestNumber(vector<int>& nums) {
-        nums = mergeSort(nums, 0, nums.size() - 1);
-        string largestNum = "";
-        for (int &num : nums) 
-            largestNum += to_string(num);
-        return largestNum[0] == '0' ? "0" : largestNum;
+    string largestNumber(vector<int>& nums) 
+    {
+        vector<string> array;
+        for (int num : nums) 
+            array.push_back(to_string(num));
+        sort(array.begin(), array.end(), [](const string &a, const string &b) {
+            return (b + a) < (a + b);
+        });
+
+        // Handle the case where the largest number is "0"
+        if (array[0] == "0") {
+            return "0";
+        }
+
+        // Build the largest number from the sorted array
+        string largest;
+        for (const string &num : array) {
+            largest += num;
+        }
+
+        return largest;
     }
 };
