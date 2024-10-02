517cb45d43f9cc842b9a6c006c7a172008e2fb50
Upamdeep Buragohain <136807333+Hengdang-Killer@users.noreply.github.com>
2024-10-02 13:33:45 +0530

    Time: 119 ms (11.09%), Space: 56.3 MB (5.75%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -3,21 +3,23 @@ class Solution
 public:
     vector<int> arrayRankTransform(vector<int>& arr) 
     {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         map<int, vector<int>> mp;
         for(int i = 0; i < arr.size(); i++)
         {
             mp[arr[i]].push_back(i);
         }
-        vector<int> ans(arr.size());
         int cnt = 1;
         for(auto itr : mp)
         {
             for(auto itr2 : itr.second)
             {
-                ans[itr2] = cnt;
+                arr[itr2] = cnt;
             }
             cnt++;
         }
-        return ans;
+        return arr;
     }
 };
